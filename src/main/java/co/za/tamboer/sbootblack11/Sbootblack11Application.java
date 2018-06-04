0d02d60797507ed4b252bacae0c8683e68f98cbb
Wil <willem.labuschagne@tvh.com>
2018-06-04 22:07:26 +0200

    first

---
+++ src/main/java/co/za/tamboer/sbootblack11/Sbootblack11Application.java
@@ -0,0 +1,12 @@
+package co.za.tamboer.sbootblack11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbootblack11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbootblack11Application.class, args);
+	}
+}
